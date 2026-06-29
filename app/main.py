67fe9e8c915eac567649284cbe90b182a57fc80b
Abhishek Kumar <itschronical99@gmail.com>
2026-06-29 12:39:09 +0530

    Day 2 FastAPI routes and Post API

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI(
-    title="SoulAI API",
-    version="1.0.0",
-    description="Backend for SoulAI"
-)
+app = FastAPI()
+
+class ChatRequest(BaseModel):
+    message:str
 
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to SoulAI 🚀",
-        "status": "Running Successfully"
+        "message": "Welcome to SoulAI 🚀"
+    }
+@app.post("/chat")
+def chat(data:ChatRequest):
+    return{
+        "user_message":data.message,
+        "reply":f"You said:{data.message}"
     }
